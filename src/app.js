0c4b0454b1854e01411b2fda64e23ff794756f87
fcoeliesio <fcoeliesio@gmail.com>
2024-06-21 15:50:38 -0300

    Setting up database connection :heavy_check_mark

--- src/app.js
+++ src/app.js
@@ -1,23 +1,21 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const process = require('dotenv').config();
 // const "requrer rotas"
-const teste = process.env.dd;
-console.log(dd);
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+
+const PORT = process.env.PORT;
+const PASSWORD = process.env.PASSWORD;
+const AAP_NAME = process.env.AAP_NAME;
+const DB_URL = `mongodb+srv://padim:${PASSWORD}@padimfood.0umakns.mongodb.net`;
 
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://bdUser:testedb@eliesio.hwrdh5h.mongodb.net',{
-  retryWrites: true,
-  w: 'majority',
-  appName:'eliesio'
-});
+mongoose.connect(DB_URL,{retryWrites: true, w: 'majority', appName: AAP_NAME});
 
 // app.use('/api', foodRouter);
 
 app.listen(PORT, ()=>{
-  console.log(`Servidor iniciado na porta ${PORT} na ${env.teste}`);
+  console.log(`Servidor iniciado na porta ${PORT}`);
 });
